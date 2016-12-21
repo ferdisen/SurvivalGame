514ace318946faa9a56a7c94d68bddbfcdf25fdf
Ferdi Şen <ferdisen1993@gmail.com>
2016-12-21 21:54:44 +0200

    C++ silindi.

--- Source/SurvivalGame/SurvivalGame.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "SurvivalGame.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivalGame, "SurvivalGame" );
